e47390f70e661756d3f6a253d839d5fd8bdd1208
Ray Corder <ccorder2@gmail.com>
2018-03-07 23:30:58 -0500

    Resolves #1: add a check on card select to see if number of estimates matches the number of players

--- src/components/PokerCard.js
+++ src/components/PokerCard.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { startSetEstimate } from '../actions/selected-work-item';
+import { startSetEstimate, startToggleVisibility } from '../actions/selected-work-item';
 
 export class PokerCard extends React.Component {
 	onClick = () => {
 		if (this.props.allowClick) {
-			this.props.startSetEstimate(this.props.card);
+			this.props.startSetEstimate(this.props.card).then(() => {
+				if (this.props.allPlayersSelected && this.props.isVisible) {
+					this.props.startToggleVisibility();
+				}
+			});
 		}
 	};
 	render() {
 		return (
 			<div
-				className={
-					this.props.isSelected && this.props.allowClick
-						? 'card card--selected'
-						: 'card'
-				}
+				className={this.props.isSelected && this.props.allowClick ? 'card card--selected' : 'card'}
 			>
 				{this.props.isVisible ? (
-					<div
-						className="card--show"
-						key={this.props.card}
-						onClick={this.onClick}
-					>
+					<div className="card--show" key={this.props.card} onClick={this.onClick}>
 						<span className="card-top">{this.props.card}</span>
 						<span className="card-main">{this.props.card}</span>
 					</div>
@@ -35,19 +31,24 @@ export class PokerCard extends React.Component {
 }
 
 const mapStateToProps = (state, props) => {
-	let item = state.games.find(game => game.id === props.gameId)
-		.selectedWorkItem;
+	let game = state.games.find(game => game.id === props.gameId);
 	return {
 		isSelected:
-			!!item && !!item.effort
-				? state.games.find(game => game.id === props.gameId).selectedWorkItem
-						.effort[state.auth.uid] === props.card
-				: false
+			!!game.selectedWorkItem && !!game.selectedWorkItem.effort
+				? game.selectedWorkItem.effort[state.auth.uid] === props.card
+				: false,
+		allPlayersSelected:
+			!!game.selectedWorkItem &&
+			!!game.selectedWorkItem.effort &&
+			!!game.players &&
+			Object.keys(game.players).length === Object.keys(game.selectedWorkItem.effort).length &&
+			!game.selectedWorkItem.showEffort.flag
 	};
 };
 
 const mapDispatchToProps = (dispatch, props) => ({
-	startSetEstimate: data => dispatch(startSetEstimate(props.gameId, data))
+	startSetEstimate: data => dispatch(startSetEstimate(props.gameId, data)),
+	startToggleVisibility: () => dispatch(startToggleVisibility(props.gameId))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(PokerCard);
